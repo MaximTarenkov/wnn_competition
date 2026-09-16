518fa0a94501d941a1ef5da023dba181b37d2e22
MaximTarenkov <glt198t@gmail.com>
2026-09-16 17:18:32 +0500

    fix: add safe mask for l1 arch

--- experiments/models/gru_mlp_encoders_l1.py
+++ experiments/models/gru_mlp_encoders_l1.py
@@ -47,8 +47,10 @@ class GRUWithEncoders(nn.Module):
         best_bid_p, bid_idx = torch.max(p_b, dim=-1, keepdim=True)
         best_bid_v = torch.gather(v_b, dim=-1, index=bid_idx)
 
-        best_ask_p, ask_idx = torch.min(p_a, dim=-1, keepdim=True)
-        best_ask_v = torch.gather(v_a, dim=-1, index=ask_idx) # Можно применить маскирование, но тут намеренно игнорируешь, так как она показала плохой результат
+        p_a_safe = torch.where(p_a <= 0.0, torch.full_like(p_a, float("inf")), p_a)
+        best_ask_p, ask_idx = torch.min(p_a_safe, dim=-1, keepdim=True)
+        best_ask_p = torch.where(torch.isinf(best_ask_p), best_bid_p, best_ask_p)
+        best_ask_v = torch.gather(v_a, dim=-1, index=ask_idx)
 
         spread = best_ask_p - best_bid_p
         mid = 0.5 * (best_bid_p + best_ask_p)
